72ae34d7d3cb85a4b72a298d59c46ff72a158cc6
SpaceGamer <83031327+Space-Gamer@users.noreply.github.com>
2022-06-14 14:47:26 +0530

    Create Selection_sort.c

---
+++ Miscellaneous/Selection_sort.c
@@ -0,0 +1,63 @@
+/*
+Program to sort an array according to selection sort algorithm
+*/
+
+#include <stdio.h>
+
+void lstp(int n, int *l)
+{
+    int i=0;
+    for (i=0;i<n;i++)
+    {
+        printf("%d ",l[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    int ar[n];
+    int i=0,j=0;
+    for (i=0;i<n;i++)
+    {
+        printf("Element %d: ",i+1);
+        scanf("%d",&ar[i]);
+    }
+    printf("Entered array is: ");
+    lstp(n,ar);
+    for (i=0;i<n;i++)
+    {
+        int s = i;
+        for (j=i+1;j<n;j++)
+        {
+            
+            if (ar[j]<ar[s])
+            {
+                s=j;
+            }
+        }
+        int t=ar[s];
+        ar[s]=ar[i];
+        ar[i]=t;
+        // lstp(n,ar); // Uncomment to get the sorting mechanism after every n iterations
+    }
+    printf("Sorted list is: ");
+    lstp(n,ar);
+}
+
+/*
+Input:
+Enter the size of array: 5
+Element 1: 63
+Element 2: -9
+Element 3: 23
+Element 4: 5
+Element 5: 10
+
+Output:
+Entered array is: 63 -9 23 5 10 
+Sorted list is: -9 5 10 23 63
+*/
